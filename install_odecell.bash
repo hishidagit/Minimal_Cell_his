21e6777b0501de96154e7d20c7a468d7642f3114
hishidagit <ahishida18@gmail.com>
2025-02-14 11:55:39 +0000

    update install odecell

--- install_odecell.bash
+++ install_odecell.bash
@@ -1,4 +1,5 @@
 # change pip version
+conda activate minimal-cell
 conda install -n minimal-cell pip==20.3.1
 
 # install libsundials-dev
@@ -9,5 +10,16 @@ pip install -r requirements.txt
 
 pip install --upgrade cython
 CFLAGS="-I$CONDA_PREFIX/include" LDFLAGS="-L$CONDA_PREFIX/lib" pip install --no-binary :all: pycvodes
+pip install importlib_resources
+
+conda install -c conda-forge pydantic=1.8.2
+
+# conda install -c conda-forge cobra
+
+conda install -c anaconda xlrd==1.2.0
+
+conda install -c anaconda mpi4py
+    
+
 
 pip install .
